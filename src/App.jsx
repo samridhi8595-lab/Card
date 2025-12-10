1be5bff683a806a30ac52d78673b287573cb08cf
samridhi8595-lab <samridhi8595@gmail.com>
2025-12-10 16:42:32 +0530

    change name and age using props

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import './App.css'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user='Samridhi' age={18} />
-      <Card user='Sarthak'/>
+      <Card user='Aman' age={18} />
+      <Card user='Sarthak' age={20}/>
+      <Card user='Rohan' age={28}/>
 
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Card = (props) => {
-    console.log(props.user,props.age)
+    
   return (
     <div className='card'>
         <img src="https://images.pexels.com/photos/1583582/pexels-photo-1583582.jpeg" />
-        <h1>Samridhi</h1>
+        <h1>{props.user},{props.age}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         <button>View Profile</button>
       
